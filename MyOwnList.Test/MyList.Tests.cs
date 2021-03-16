df38f792076ae7ba907623012bb5d1a14a0ba210
Ola <oleksandrakondratenko@gmail.com>
2021-03-16 16:40:53 +0200

    Added test for Min, Max, Min/MaxPos methods

--- MyOwnList.Test/MyList.Tests.cs
+++ MyOwnList.Test/MyList.Tests.cs
@@ -52,5 +52,56 @@ namespace MyOwnList.Test
             new MyList<int>() { 1, 2, 3, 4, 5, 6, 10} };
         }
 
+        [TestCaseSource(nameof(DataMaxPosTest))]
+        public void MaxPos_WhenInputIsValued_ShouldFindIndexMaxElement( MyList<int> result, int posExpected)
+        {
+            int posResult = result.MaxPos();
+
+            Assert.AreEqual(posExpected, posResult);
+        }
+
+        private static IEnumerable<object[]> DataMaxPosTest()
+        {
+            yield return new object[] { new MyList<int>() { 1, 2, 10, 4, 5, 6 }, 2 };
+        }
+
+        [TestCaseSource(nameof(DataMinPosTest))]
+        public void MinPos_WhenInputIsValued_ShouldFindIndexMinElement(MyList<int> result, int posExpected)
+        {
+            int posResult = result.MinPos();
+
+            Assert.AreEqual(posExpected, posResult);
+        }
+
+        private static IEnumerable<object[]> DataMinPosTest()
+        {
+            yield return new object[] { new MyList<int>() { 1, 2, 10, 0, 4, 5, 6 }, 3 };
+        }
+
+        [TestCaseSource(nameof(DataMaxTest))]
+        public void Max_WhenInputIsValued_ShouldFindMaxElement(MyList<int> result, int posExpected)
+        {
+            int posResult = result.Max();
+
+            Assert.AreEqual(posExpected, posResult);
+        }
+
+        private static IEnumerable<object[]> DataMaxTest()
+        {
+            yield return new object[] { new MyList<int>() { 1, 2, 10, 4, 5, 6 }, 10 };
+        }
+
+        [TestCaseSource(nameof(DataMinTest))]
+        public void Min_WhenInputIsValued_ShouldFindMaxElement(MyList<int> result, int posExpected)
+        {
+            int posResult = result.Min();
+
+            Assert.AreEqual(posExpected, posResult);
+        }
+
+        private static IEnumerable<object[]> DataMinTest()
+        {
+            yield return new object[] { new MyList<int>() { 1, 2, 10, 0, 4, 5, 6 }, 0 };
+        }
     }
 }
